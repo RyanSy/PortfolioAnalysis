037ddc0c45d979517c744e68e5b783d38e2b291e
Ryan Sy <ryanbsy@gmail.com>
2025-11-25 12:19:23 -0500

    fixed mistake in create_engine in helpers

--- jupyter_notebooks/utils.py
+++ jupyter_notebooks/utils.py
@@ -26,7 +26,7 @@ DB_PORT = os.getenv('DB_PORT')
 DB_NAME = os.getenv('DB_NAME')
 DB_URL = f'{DB_DIALECT}+{DB_DRIVER}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
-engine = create_engine(os.getenv('DATABASE_URL'))
+engine = create_engine(DB_URL)
 
 
 def run_sql(log_msg :str, sql_query: str, data=None) -> None:
